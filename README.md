736e55f2437cce2a152f65068cc7f2f2a92739e8
egxn <afvelosas@correo.udistrital.edu.co>
2024-02-15 21:50:43 -0500

    Dynamic file server

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 - PadddleOCR
 - Roboflow
 
+# To Do
+
+* Create a Rust package and add https://mvdnes.github.io/rust-docs/zip-rs/zip/read/struct.ZipArchive.html
+* Create a HTML viewer with ordered pages and tab navigation  (Maybe use Tauri)
+* Create a FastAPI server to PadddleOCR, Roboflow, and CoquiTTS
+ 
---
+++ skunk-back/src/main.rs
@@ -0,0 +1,63 @@
+use axum::Router;
+use axum::routing::get;
+use std::{env, fs::File};
+use std::path::Path;
+use zip::read::ZipArchive;
+
+fn uncompress (path: &str) -> &str {
+    let file = File::open(path)
+        .expect("Failed to open file");
+    let mut archive = ZipArchive::new(file)
+        .expect("Failed to read zip");
+    
+    let stem = Path::new(path).file_stem().unwrap().to_str().unwrap();
+    std::fs::create_dir_all(stem)
+        .expect("Failed to create dir");
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+        let outpath = Path::new(stem).join(file.name());
+        let mut outfile = File::create(&outpath).unwrap();
+        std::io::copy(&mut file, &mut outfile).unwrap();
+        print!("File {}: File extracted\n", file.name());
+    }
+
+    stem
+}
+
+fn get_file_path () -> Result<String, Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args()
+        .collect();
+    if args.len() < 2 {
+        println!("Please provide a file path as an argument");
+        Err("No file path provided")?;
+    }
+
+    let path = &args[1];
+    Ok(path.to_string())
+}
+
+async fn root() -> &'static str {
+    "Skunk!"
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    let path = get_file_path();
+    match path {
+        Ok(path) => {
+            let stem = uncompress(&path);
+            let path = format!("/{}", stem);
+            let app = Router::new()
+                .route("/", get(root))
+                .nest(path.as_str(), axum_static::static_router(stem));
+
+            axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+                .serve(app.into_make_service())
+                .await
+                .unwrap();
+        },
+        Err(e) => println!("Error: {}", e)
+    }
+}
